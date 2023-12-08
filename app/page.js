195191110ff28b8d0f5db843084d0b45a706a3e5
abdulla <abdullahsmsapk@gmail.com>
2023-12-08 22:50:34 +0530

    fixes

--- app/page.js
+++ app/page.js
@@ -1,18 +1,6 @@
-'use client'  
 import LandingPage from './LandingPage';
-
 export default function coe() {
- 
-
   return (
     <LandingPage/>
-    // <div className='login-container'>
-    //   <div className="d-flex flex-column p-3 text-center back-clr"> 
-    //     <h3>Login</h3> 
-    //     <input type="text" className='p-2 m-2' placeholder='Username'/>
-    //     <input type="text" className='p-2 m-2' placeholder='Password'/>
-    //     <button className='primary-clr p-2 m-2' onClick={()=>handleClick()}>Submit</button>
-    //   </div>
-    // </div>
   );
 }
